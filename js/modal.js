779537c8192930e5ec1c0076127debc4f391b0aa
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2023-11-21 12:08:43 +0100

    position

---
+++ js/modal.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector('.backdrop');
+const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnClose = document.querySelector('.modal-btn-close');
+
+console.log(modal)
+
+console.log(modalBtnOpen)
+console.log(modalBtnClose)
+
+const toggleModal = () => modal.classList.toggle('is-hidden');
+
+modalBtnOpen.addEventListener('click',toggleModal);
+modalBtnClose.addEventListener('click',toggleModal);
