bdea3c9bf44602f33c57ee99f511476d7298297a
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2023-11-25 21:43:14 +0100

    mobile adaptive page

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,11 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnOpen = document.querySelectorAll('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal__btn-close');
 
-console.log(modal)
-
-console.log(modalBtnOpen)
-console.log(modalBtnClose)
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('click',toggleModal);
+modalBtnOpen.forEach(btn => {
+    btn.addEventListener('click', toggleModal);
+});
 modalBtnClose.addEventListener('click',toggleModal);
